3f82a9ff641f9623d1bc603b1e1e9eb1852586aa
JordyMC86 <144482108+JordyMC86@users.noreply.github.com>
2024-10-21 14:35:49 -0500

    Carrito

--- app/src/main/java/com/example/tiendavirtual/ListadoProductosActivity.java
+++ app/src/main/java/com/example/tiendavirtual/ListadoProductosActivity.java
@@ -1,6 +1,9 @@
 package com.example.tiendavirtual;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ListadoProductosActivity extends AppCompatActivity {
-
+    private Button CarritoBotton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,16 @@ public class ListadoProductosActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        CarritoBotton = findViewById(R.id.imageFilterButton);
+
+        CarritoBotton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListadoProductosActivity.this, Carrito.class);
+                startActivity(intent);
+            }
         });
     }
 }
