ad425b940b49c9bbbc554a2d6e46e168e96935e8
JordyMC86 <144482108+JordyMC86@users.noreply.github.com>
2024-11-05 15:25:03 -0500

    avance geolocalizacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,10 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,6 +41,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation ("com.google.android.gms:play-services-analytics-impl-license:12.0.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
